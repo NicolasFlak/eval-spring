c56f169f5cd4388584a34157b5a0254601b083e4
Nicolas <punish.yourself@hotmail.fr>
2023-02-13 13:28:19 +0100

    config projet

---
+++ src/main/java/hb/spring/evaluation/SpringEvaluationApplication.java
@@ -0,0 +1,14 @@
+package hb.spring.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEvaluationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringEvaluationApplication.class, args);
+	}
+
+}
